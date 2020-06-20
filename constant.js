d72811706d726383722e83f323666e55062d844d
Le Truong An <anletr97@gmail.com>
2020-06-20 13:16:13 +0700

    update schedule

--- constant.js
+++ constant.js
@@ -1,6 +1,10 @@
 const BOT_TOKEN = 'NzE5MTE2MDM2NzA3ODQ0MTM2.Xu2cvw.2-yD4eNucR8vSFhGpWdpkJ01DmA';
 const CHANNEL_ID = '719114527643402252';
-const SCHEDULE_TIME = "*/3600 * * * * *";
+
+/**
+ * “At minute 1 past every 2nd hour from 0 through 23 on every day-of-week from Monday through Sunday.”
+ */
+const SCHEDULE_TIME = "1 0-23/2 * * 1-7";
 
 module.exports = {
     BOT_TOKEN,
